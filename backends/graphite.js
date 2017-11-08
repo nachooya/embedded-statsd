ac752ef304e8b43e264769878fa433df42540232
nacho <nachooya@gmail.com>
2017-11-08 08:59:18 -0300

    Make graphite backend independant from instance

--- backends/graphite.js
+++ backends/graphite.js
@@ -20,240 +20,241 @@
 
 var net = require('net');
 
-// this will be instantiated to the logger
-var l;
+exports.init = function graphite_init(startup_time, config, events, logger) {
 
-var debug;
-var flushInterval;
-var graphiteHost;
-var graphitePort;
-var graphitePicklePort;
-var graphiteProtocol;
-var flush_counts;
+  // this will be instantiated to the logger
+  var l;
 
-// prefix configuration
-var globalPrefix;
-var prefixCounter;
-var prefixTimer;
-var prefixGauge;
-var prefixSet;
-var globalSuffix;
-var prefixStats;
-var globalKeySanitize = true;
+  var debug;
+  var flushInterval;
+  var graphiteHost;
+  var graphitePort;
+  var graphitePicklePort;
+  var graphiteProtocol;
+  var flush_counts;
 
-// set up namespaces
-var legacyNamespace  = true;
-var globalNamespace  = [];
-var counterNamespace = [];
-var timerNamespace   = [];
-var gaugesNamespace  = [];
-var setsNamespace    = [];
+  // prefix configuration
+  var globalPrefix;
+  var prefixCounter;
+  var prefixTimer;
+  var prefixGauge;
+  var prefixSet;
+  var globalSuffix;
+  var prefixStats;
+  var globalKeySanitize = true;
 
-var graphiteStats = {};
+  // set up namespaces
+  var legacyNamespace  = true;
+  var globalNamespace  = [];
+  var counterNamespace = [];
+  var timerNamespace   = [];
+  var gaugesNamespace  = [];
+  var setsNamespace    = [];
 
-var post_stats = function graphite_post_stats(stats) {
-  var last_flush = graphiteStats.last_flush || 0;
-  var last_exception = graphiteStats.last_exception || 0;
-  var flush_time = graphiteStats.flush_time || 0;
-  var flush_length = graphiteStats.flush_length || 0;
+  var graphiteStats = {};
 
-  if (graphiteHost) {
-    try {
-      var port = graphiteProtocol == 'pickle' ? graphitePicklePort : graphitePort;
-      var graphite = net.createConnection(port, graphiteHost);
-      graphite.addListener('error', function(connectionException){
-        l.log('[graphite]: conneciont exception: '+connectionException);
-      });
-      graphite.on('connect', function() {
-        var ts = Math.round(Date.now() / 1000);
-        var namespace = globalNamespace.concat(prefixStats).join(".");
-        stats.add(namespace + '.graphiteStats.last_exception' + globalSuffix, last_exception, ts);
-        stats.add(namespace + '.graphiteStats.last_flush'     + globalSuffix, last_flush    , ts);
-        stats.add(namespace + '.graphiteStats.flush_time'     + globalSuffix, flush_time    , ts);
-        stats.add(namespace + '.graphiteStats.flush_length'   + globalSuffix, flush_length  , ts);
-        var stats_payload = graphiteProtocol == 'pickle' ? stats.toPickle() : stats.toText();
+  var post_stats = function graphite_post_stats(stats) {
+    var last_flush = graphiteStats.last_flush || 0;
+    var last_exception = graphiteStats.last_exception || 0;
+    var flush_time = graphiteStats.flush_time || 0;
+    var flush_length = graphiteStats.flush_length || 0;
 
-        var starttime = Date.now();
-        this.write(stats_payload);
-        this.end();
+    if (graphiteHost) {
+      try {
+        var port = graphiteProtocol == 'pickle' ? graphitePicklePort : graphitePort;
+        var graphite = net.createConnection(port, graphiteHost);
+        graphite.addListener('error', function(connectionException){
+          l.log('[graphite]: conneciont exception: '+connectionException);
+        });
+        graphite.on('connect', function() {
+          var ts = Math.round(Date.now() / 1000);
+          var namespace = globalNamespace.concat(prefixStats).join(".");
+          stats.add(namespace + '.graphiteStats.last_exception' + globalSuffix, last_exception, ts);
+          stats.add(namespace + '.graphiteStats.last_flush'     + globalSuffix, last_flush    , ts);
+          stats.add(namespace + '.graphiteStats.flush_time'     + globalSuffix, flush_time    , ts);
+          stats.add(namespace + '.graphiteStats.flush_length'   + globalSuffix, flush_length  , ts);
+          var stats_payload = graphiteProtocol == 'pickle' ? stats.toPickle() : stats.toText();
 
-        graphiteStats.flush_time = (Date.now() - starttime);
-        graphiteStats.flush_length = stats_payload.length;
-        graphiteStats.last_flush = Math.round(Date.now() / 1000);
-      });
-    } catch(e){
-      l.log('[graphite]: exception: '+e);
-      graphiteStats.last_exception = Math.round(Date.now() / 1000);
-    }
-  }
-};
+          var starttime = Date.now();
+          this.write(stats_payload);
+          this.end();
 
-// Minimally necessary pickle opcodes.
-var MARK = '(',
-    STOP = '.',
-    LONG = 'L',
-    STRING = 'S',
-    APPEND = 'a',
-    LIST = 'l',
-    TUPLE = 't';
-
-// A single measurement for sending to graphite.
-function Metric(key, value, ts) {
-  var m = this;
-  this.key = key;
-  this.value = value;
-  this.ts = ts;
-
-  // return a string representation of this metric appropriate 
-  // for sending to the graphite collector. does not include
-  // a trailing newline.
-  this.toText = function() {
-    return m.key + " " + m.value + " " + m.ts;
-  };
-
-  this.toPickle = function() {
-    return MARK + STRING + '\'' + m.key + '\'\n' + MARK + LONG + m.ts + 'L\n' + STRING + '\'' + m.value + '\'\n' + TUPLE + TUPLE + APPEND;
-  };
-}
-
-// A collection of measurements for sending to graphite.
-function Stats() {
-  var s = this;
-  this.metrics = [];
-  this.add = function(key, value, ts) {
-    s.metrics.push(new Metric(key, value, ts));
-  };
-
-  this.toText = function() {
-    return s.metrics.map(function(m) { return m.toText(); }).join('\n') + '\n';
-  };
-
-  this.toPickle = function() {
-    var body = MARK + LIST + s.metrics.map(function(m) { return m.toPickle(); }).join('') + STOP;
-
-    // The first four bytes of the graphite pickle format
-    // contain the length of the rest of the payload.
-    // We use Buffer because this is binary data.
-    var buf = new Buffer(4 + body.length);
-
-    buf.writeUInt32BE(body.length,0);
-    buf.write(body,4);
-
-    return buf;
-  };
-}
-
-var flush_stats = function graphite_flush(ts, metrics) {
-  var starttime = Date.now();
-  var numStats = 0;
-  var key;
-  var timer_data_key;
-  var counters = metrics.counters;
-  var gauges = metrics.gauges;
-  var timers = metrics.timers;
-  var sets = metrics.sets;
-  var counter_rates = metrics.counter_rates;
-  var timer_data = metrics.timer_data;
-  var statsd_metrics = metrics.statsd_metrics;
-
-  // Sanitize key for graphite if not done globally
-  function sk(key) {
-    if (globalKeySanitize) {
-      return key;
-    } else {
-      return key.replace(/\s+/g, '_')
-                .replace(/\//g, '-')
-                .replace(/[^a-zA-Z_\-0-9\.]/g, '');
-    }
-  };
-
-  // Flatten all the different types of metrics into a single
-  // collection so we can allow serialization to either the graphite
-  // text and pickle formats.
-  var stats = new Stats();
-
-  for (key in counters) {
-    var value = counters[key];
-    var valuePerSecond = counter_rates[key]; // pre-calculated "per second" rate
-    var keyName = sk(key);
-    var namespace = counterNamespace.concat(keyName);
-
-    if (legacyNamespace === true) {
-      stats.add(namespace.join(".") + globalSuffix, valuePerSecond, ts);
-      if (flush_counts) {
-        stats.add('stats_counts.' + keyName + globalSuffix, value, ts);
-      }
-    } else {
-      stats.add(namespace.concat('rate').join(".")  + globalSuffix, valuePerSecond, ts);
-      if (flush_counts) {
-        stats.add(namespace.concat('count').join(".") + globalSuffix, value, ts);
+          graphiteStats.flush_time = (Date.now() - starttime);
+          graphiteStats.flush_length = stats_payload.length;
+          graphiteStats.last_flush = Math.round(Date.now() / 1000);
+        });
+      } catch(e){
+        l.log('[graphite]: exception: '+e);
+        graphiteStats.last_exception = Math.round(Date.now() / 1000);
       }
     }
+  };
 
-    numStats += 1;
+  // Minimally necessary pickle opcodes.
+  var MARK = '(',
+      STOP = '.',
+      LONG = 'L',
+      STRING = 'S',
+      APPEND = 'a',
+      LIST = 'l',
+      TUPLE = 't';
+
+  // A single measurement for sending to graphite.
+  function Metric(key, value, ts) {
+    var m = this;
+    this.key = key;
+    this.value = value;
+    this.ts = ts;
+
+    // return a string representation of this metric appropriate
+    // for sending to the graphite collector. does not include
+    // a trailing newline.
+    this.toText = function() {
+      return m.key + " " + m.value + " " + m.ts;
+    };
+
+    this.toPickle = function() {
+      return MARK + STRING + '\'' + m.key + '\'\n' + MARK + LONG + m.ts + 'L\n' + STRING + '\'' + m.value + '\'\n' + TUPLE + TUPLE + APPEND;
+    };
   }
 
-  for (key in timer_data) {
-    var namespace = timerNamespace.concat(sk(key));
-    var the_key = namespace.join(".");
+  // A collection of measurements for sending to graphite.
+  function Stats() {
+    var s = this;
+    this.metrics = [];
+    this.add = function(key, value, ts) {
+      s.metrics.push(new Metric(key, value, ts));
+    };
 
-    for (timer_data_key in timer_data[key]) {
-      if (typeof(timer_data[key][timer_data_key]) === 'number') {
-        stats.add(the_key + '.' + timer_data_key + globalSuffix, timer_data[key][timer_data_key], ts);
+    this.toText = function() {
+      return s.metrics.map(function(m) { return m.toText(); }).join('\n') + '\n';
+    };
+
+    this.toPickle = function() {
+      var body = MARK + LIST + s.metrics.map(function(m) { return m.toPickle(); }).join('') + STOP;
+
+      // The first four bytes of the graphite pickle format
+      // contain the length of the rest of the payload.
+      // We use Buffer because this is binary data.
+      var buf = new Buffer(4 + body.length);
+
+      buf.writeUInt32BE(body.length,0);
+      buf.write(body,4);
+
+      return buf;
+    };
+  }
+
+  var flush_stats = function graphite_flush(ts, metrics) {
+    var starttime = Date.now();
+    var numStats = 0;
+    var key;
+    var timer_data_key;
+    var counters = metrics.counters;
+    var gauges = metrics.gauges;
+    var timers = metrics.timers;
+    var sets = metrics.sets;
+    var counter_rates = metrics.counter_rates;
+    var timer_data = metrics.timer_data;
+    var statsd_metrics = metrics.statsd_metrics;
+
+    // Sanitize key for graphite if not done globally
+    function sk(key) {
+      if (globalKeySanitize) {
+        return key;
       } else {
-        for (var timer_data_sub_key in timer_data[key][timer_data_key]) {
-          if (debug) {
-            l.log(timer_data[key][timer_data_key][timer_data_sub_key].toString());
-          }
-          stats.add(the_key + '.' + timer_data_key + '.' + timer_data_sub_key + globalSuffix,
-                    timer_data[key][timer_data_key][timer_data_sub_key], ts);
+        return key.replace(/\s+/g, '_')
+                  .replace(/\//g, '-')
+                  .replace(/[^a-zA-Z_\-0-9\.]/g, '');
+      }
+    };
+
+    // Flatten all the different types of metrics into a single
+    // collection so we can allow serialization to either the graphite
+    // text and pickle formats.
+    var stats = new Stats();
+
+    for (key in counters) {
+      var value = counters[key];
+      var valuePerSecond = counter_rates[key]; // pre-calculated "per second" rate
+      var keyName = sk(key);
+      var namespace = counterNamespace.concat(keyName);
+
+      if (legacyNamespace === true) {
+        stats.add(namespace.join(".") + globalSuffix, valuePerSecond, ts);
+        if (flush_counts) {
+          stats.add('stats_counts.' + keyName + globalSuffix, value, ts);
+        }
+      } else {
+        stats.add(namespace.concat('rate').join(".")  + globalSuffix, valuePerSecond, ts);
+        if (flush_counts) {
+          stats.add(namespace.concat('count').join(".") + globalSuffix, value, ts);
         }
       }
+
+      numStats += 1;
     }
-    numStats += 1;
-  }
 
-  for (key in gauges) {
-    var namespace = gaugesNamespace.concat(sk(key));
-    stats.add(namespace.join(".") + globalSuffix, gauges[key], ts);
-    numStats += 1;
-  }
+    for (key in timer_data) {
+      var namespace = timerNamespace.concat(sk(key));
+      var the_key = namespace.join(".");
 
-  for (key in sets) {
-    var namespace = setsNamespace.concat(sk(key));
-    stats.add(namespace.join(".") + '.count' + globalSuffix, sets[key].size(), ts);
-    numStats += 1;
-  }
-
-  if (legacyNamespace === true) {
-    stats.add(prefixStats + '.numStats' + globalSuffix, numStats, ts);
-    stats.add('stats.' + prefixStats + '.graphiteStats.calculationtime' + globalSuffix, (Date.now() - starttime), ts);
-    for (key in statsd_metrics) {
-      stats.add('stats.' + prefixStats + '.' + key + globalSuffix, statsd_metrics[key], ts);
+      for (timer_data_key in timer_data[key]) {
+        if (typeof(timer_data[key][timer_data_key]) === 'number') {
+          stats.add(the_key + '.' + timer_data_key + globalSuffix, timer_data[key][timer_data_key], ts);
+        } else {
+          for (var timer_data_sub_key in timer_data[key][timer_data_key]) {
+            if (debug) {
+              l.log(timer_data[key][timer_data_key][timer_data_sub_key].toString());
+            }
+            stats.add(the_key + '.' + timer_data_key + '.' + timer_data_sub_key + globalSuffix,
+                      timer_data[key][timer_data_key][timer_data_sub_key], ts);
+          }
+        }
+      }
+      numStats += 1;
     }
-  } else {
-    var namespace = globalNamespace.concat(prefixStats);
-    stats.add(namespace.join(".") + '.numStats' + globalSuffix, numStats, ts);
-    stats.add(namespace.join(".") + '.graphiteStats.calculationtime' + globalSuffix, (Date.now() - starttime) , ts);
-    for (key in statsd_metrics) {
-      var the_key = namespace.concat(key);
-      stats.add(the_key.join(".") + globalSuffix,+ statsd_metrics[key], ts);
+
+    for (key in gauges) {
+      var namespace = gaugesNamespace.concat(sk(key));
+      stats.add(namespace.join(".") + globalSuffix, gauges[key], ts);
+      numStats += 1;
     }
-  }
-  post_stats(stats);
 
-  if (debug) {
-   l.log("numStats: " + numStats);
-  }
-};
+    for (key in sets) {
+      var namespace = setsNamespace.concat(sk(key));
+      stats.add(namespace.join(".") + '.count' + globalSuffix, sets[key].size(), ts);
+      numStats += 1;
+    }
 
-var backend_status = function graphite_status(writeCb) {
-  for (var stat in graphiteStats) {
-    writeCb(null, 'graphite', stat, graphiteStats[stat]);
-  }
-};
+    if (legacyNamespace === true) {
+      stats.add(prefixStats + '.numStats' + globalSuffix, numStats, ts);
+      stats.add('stats.' + prefixStats + '.graphiteStats.calculationtime' + globalSuffix, (Date.now() - starttime), ts);
+      for (key in statsd_metrics) {
+        stats.add('stats.' + prefixStats + '.' + key + globalSuffix, statsd_metrics[key], ts);
+      }
+    } else {
+      var namespace = globalNamespace.concat(prefixStats);
+      stats.add(namespace.join(".") + '.numStats' + globalSuffix, numStats, ts);
+      stats.add(namespace.join(".") + '.graphiteStats.calculationtime' + globalSuffix, (Date.now() - starttime) , ts);
+      for (key in statsd_metrics) {
+        var the_key = namespace.concat(key);
+        stats.add(the_key.join(".") + globalSuffix,+ statsd_metrics[key], ts);
+      }
+    }
+    post_stats(stats);
 
-exports.init = function graphite_init(startup_time, config, events, logger) {
+    if (debug) {
+        l.log("numStats: " + numStats);
+    }
+  };
+
+  var backend_status = function graphite_status(writeCb) {
+    for (var stat in graphiteStats) {
+      writeCb(null, 'graphite', stat, graphiteStats[stat]);
+    }
+  };
+
   debug = config.debug;
   l = logger;
   graphiteHost = config.graphiteHost;
